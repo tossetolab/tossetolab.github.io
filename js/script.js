8e9f78c1d293d5af43c90881decb40a7554f80a5
Toshikazu SETO <tosseto@gmail.com>
2022-01-07 14:24:23 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  //ページ内スクロール
+  var $nav = $(".header");
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
